bf7ebdd2f0e6727c1c0263e85d8a31a9581bbc0e
BKSaint <113442572+BKSaint@users.noreply.github.com>
2022-12-15 10:54:58 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-levelupfile = open("levelup.txt")
+# levelupfile = open("levelup.txt")
 
 
-try:
-    for lines in levelupfile:
-        # x = lines.split("")[0]
-        # ipaddresses.append(x) 
+# try:
+#     for lines in levelupfile:
+#         # x = lines.split("")[0]
+#         # ipaddresses.append(x) 
         
-except:
-    print("stop")
+# except:
+#     print("stop")
 
-print(ipaddresses)
+# print(ipaddresses)
