c3079df3060b9dff2656bf92966f9bc0b9b215d2
BKSaint <113442572+BKSaint@users.noreply.github.com>
2022-12-15 10:48:45 -0500

    ip, dates, files

---
+++ test.py
@@ -0,0 +1,12 @@
+levelupfile = open("levelup.txt")
+
+
+try:
+    for lines in levelupfile:
+        # x = lines.split("")[0]
+        # ipaddresses.append(x) 
+        
+except:
+    print("stop")
+
+print(ipaddresses)
